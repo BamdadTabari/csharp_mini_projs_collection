e05e96fe377a686254cea6cecf8175f1eb632c14
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-01-15 11:01:17 -0800

    Add project files.

---
+++ SimpleTelBot/Handlers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace SimpleTelBot
+{
+
+    public class Handlers
+    {
+      
+    }
+}
+
---
+++ SimpleTelBot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+
+namespace SimpleTelBot
+{
+    class Program
+    {
+        private static TelegramBotClient Bot;
+        static async Task Main(string[] args)
+        {
+            Bot = new TelegramBotClient("5041987467:AAEF6Jr5z8i9dkbfs6x-qhhs72raHftnFfE");
+            User me = await Bot.GetMeAsync();
+            Console.Title = me.Username ?? "My awesome Bot";
+
+            using var cts = new CancellationTokenSource();
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            //ReceiverOptions receiverOptions = new() { AllowedUpdates = { } };
+            //Bot.StartReceiving(Handlers.HandleUpdateAsync,
+            //                   Handlers.HandleErrorAsync,
+            //                   receiverOptions,
+            //                   cts.Token);
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            // Send cancellation request to stop bot
+            cts.Cancel();
+        }
+    }
+}
